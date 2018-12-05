0d14b8c9bdae00c88a38e1ce562c4e7c09a0ef7f
sakwafrancis89@gmail.com <sakwafrancis89@gmail.com>
2018-12-05 13:05:36 +0300

    styled

--- news/tests.py
+++ news/tests.py
@@ -28,11 +28,11 @@ class ArticleTestClass(TestCase):
 
     def setUp(self):
         # Creating a new editor and saving it
-        self.james= Editor(first_name = 'James', last_name ='Muriuki', email ='james@moringaschool.com')
+        self.james= Editor(first_name = 'James', last_name ='Muruiki', email ='james@moringaschool.com')
         self.james.save_editor()
 
         # Creating a new tag and saving it
-        self.new_tag = tags(name = 'testing')
+        self.new_tag = tags(name = '')
         self.new_tag.save()
 
         self.new_article= Article(title = 'Test Article',post = 'This is a random test Post',editor = self.james)
